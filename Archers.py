34edcec6e69d7e2bd69191e1d015934c6486c82d
hicksonjp <56277359+hicksonjp@users.noreply.github.com>
2019-10-26 13:24:25 +0100

    Added comments

--- Archers.py
+++ Archers.py
@@ -1,3 +1,23 @@
+# The Archers Spoilers
+# ====================
+#
+#   By John Hickson
+#
+# Use the BBC R4 website to list upcoming Archers episodes 
+#
+# This software is written on Python 3. makes use of the 
+# BeautifulSoup (v4.4.1), a Python library which is used to "scrape" 
+# the one-line spoiler text from the BBC Radio 4 schedules website.  
+# BeautifulSoup can be found here: 
+# 
+# https://www.crummy.com/software/BeautifulSoup/
+#
+# In the file Archers.py, the variable SCHEDULE_URL holds the URL for 
+# the BBC radio 4 schedules.  The "random" charachers in the last part of 
+# the URL may need updating from time to time.  The latest URL can found 
+# by going to the website using a browser.
+#
+
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
